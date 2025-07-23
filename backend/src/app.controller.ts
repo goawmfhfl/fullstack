82455ac49173e3e50d2b8db74c6eb597723844a6
goawmfhfl <goawmfhfl1@naver.com>
2025-07-23 16:49:28 +0900

    init: inflearn 폴더 전체 업로드

---
+++ backend/src/app.controller.ts
@@ -0,0 +1,19 @@
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+import { PrismaClient } from 'generated/prisma';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    const prisma = new PrismaClient();
+    prisma.test.create({
+      data: {
+        id: '1',
+      },
+    });
+    return this.appService.getHello();
+  }
+}
